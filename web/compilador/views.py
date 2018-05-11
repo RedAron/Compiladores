fca7b5565cd689aa131775d0bf951c7592558bf6
janer gonzalez <janer369@gmail.com>
2018-05-11 15:02:36 -0500

    realizando las cargas iniciales

--- web/compilador/views.py
+++ web/compilador/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
+from  django.shortcuts import render
+
+
 # Create your views here.
+
+def hello_world(request):
+	#ordenar por id
+	template=loader.get_template('index.html')
+	
+	return HttpResponse(template.render())
+
